c2e9ca0aac09ac1f880ed1bb5f5d17861d0b47bb
Steven <steven.desvars@gmail.com>
2019-12-23 11:42:09 -0300

    Completed the about_triangle_project koan

--- python2/koans/triangle.py
+++ python2/koans/triangle.py
@@ -18,7 +18,12 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
-    # DELETE 'PASS' AND WRITE THIS CODE
+    if (a == b == c):
+        return 'equilateral'
+    elif (a == b or a == c or b == c):
+        return 'isosceles'
+    else:
+        return 'scalene'
     pass
 
 
